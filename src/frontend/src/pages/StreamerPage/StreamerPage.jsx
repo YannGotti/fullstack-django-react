cf931fa3d26e359c0e795930596bd5f0298acbf8
mironovv <1111111111aleksey46@gmail.com>
2024-03-21 01:55:42 +0300

    add css styles

--- src/frontend/src/pages/StreamerPage/StreamerPage.jsx
+++ src/frontend/src/pages/StreamerPage/StreamerPage.jsx
@@ -24,7 +24,7 @@ export default function StreamerPage() {
 
       <div className="container">
         <div className="row d-flex justify-content-center text-light row-height">
-          <div className="col-12 col-lg-8 text-center d-flex align-self-end">
+          <div className="col-12 col-lg-8 text-center d-flex align-self-end justify-content-center">
             <video
               className="video-streamer"
               src={streamer.video_preview}
@@ -33,41 +33,54 @@ export default function StreamerPage() {
               autoPlay
             ></video>
           </div>
-          <div className="col-12 col-lg-4 text-start">
-            <h3 className="text-h3-custom">{streamer.name}</h3>
-            <h3 className="text-h4-custom">{streamer.fullname}</h3>
-            <h3 className="text-h4-custom">Возраст: {streamer.age}</h3>
-            <h3 className="text-h4-custom">{streamer.description}</h3>
+          <div className="col-12 col-lg-4 text-start d-flex align-self-center">
+            <div className="vstack gap-2">
+              <div className="hstack gap-1 wrapper p-1 mt-3 d-flex justify-content-center">
+                <a
+                  href={streamer.instagram_url}
+                  class="icon instagram"
+                  target="_blank"
+                >
+                  <div class="tooltip">Instagram</div>
+                  <span className="d-flex justify-content-center mt-3">
+                    <i class="fab fa-instagram"></i>
+                  </span>
+                </a>
 
-            <div className="hstack gap-1 wrapper">
-              <a
-                href={streamer.instagram_url}
-                class="icon instagram"
-                target="_blank"
-              >
-                <div class="tooltip">Instagram</div>
-                <span className="d-flex justify-content-center mt-3">
-                  <i class="fab fa-instagram"></i>
-                </span>
-              </a>
+                <a
+                  href={streamer.youtube_url}
+                  class="icon youtube"
+                  target="_blank"
+                >
+                  <div class="tooltip">Youtube</div>
+                  <span className="d-flex align-self-center justify-content-center mt-3">
+                    <i class="fab fa-youtube"></i>
+                  </span>
+                </a>
 
-              <a
-                href={streamer.youtube_url}
-                class="icon youtube"
-                target="_blank"
-              >
-                <div class="tooltip">Youtube</div>
-                <span className="d-flex align-self-center justify-content-center mt-3">
-                  <i class="fab fa-youtube"></i>
-                </span>
-              </a>
+                <a
+                  href={streamer.twitch_url}
+                  class="icon twitch"
+                  target="_blank"
+                >
+                  <div class="tooltip">Twitch</div>
+                  <span className="d-flex align-self-center justify-content-center mt-3">
+                    <i class="fab fa-twitch"></i>
+                  </span>
+                </a>
+              </div>
 
-              <a href={streamer.twitch_url} class="icon twitch" target="_blank">
-                <div class="tooltip">Twitch</div>
-                <span className="d-flex align-self-center justify-content-center mt-3">
-                  <i class="fab fa-twitch"></i>
-                </span>
-              </a>
+              <div className="info-panel p-3">
+                <a
+                  href={streamer.twitch_url}
+                  className="text-h2-custom m-2 link-streamer"
+                >
+                  {streamer.name}
+                </a>
+                <h3 className="text-h4-custom m-2 mt-4">{streamer.fullname}</h3>
+                <h3 className="text-h4-custom m-2">Возраст: {streamer.age}</h3>
+                <h3 className="text-h5-custom m-2">{streamer.description}</h3>
+              </div>
             </div>
           </div>
         </div>
